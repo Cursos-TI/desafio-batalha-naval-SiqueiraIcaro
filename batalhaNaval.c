f2071e3b0d36ffe5019f6031bb2962e318a3d587
SiqueiraIcaro <siqueiraicaro2002@gmail.com>
2024-10-05 01:52:36 +0000

    Fiz o posicionamento dos navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,34 +7,45 @@
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
+
+
+    int tamanho_navio_horizontal = 3;
+    int tamanho_navio_vertical = 4;
+    int tabuleiro[10][10];
+
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
+    int x_hor = 1;
+    int y_hor = 2;
+
+    int x_ver = 6;
+    int y_ver = 4;
+
+    for (int i = 0; i < tamanho_navio_horizontal; i++)
+    {
+        tabuleiro[y_hor][x_hor + i] = 'N';
+    }
+
+    for (int i = 0; i < tamanho_navio_horizontal; i++)
+    {
+        tabuleiro[y_ver][x_ver + i] = 'n';
+    }
+
+
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    printf("Coordenadas do navio horizontal:\n");
+    for (int i = 0; i < tamanho_navio_horizontal; i++) 
+    {
+        printf("(%d, %d)\n", x_hor + i, y_hor);
+    }
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    printf("Coordenadas do navio vertical:\n");
+    for (int i = 0; i < tamanho_navio_vertical; i++) 
+    {
+        printf("(%d, %d)\n", x_hor + i, y_hor);
+    }
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
